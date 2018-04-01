729b333f3da110b0c18da90115d57932715c8d14
alex-scp <36778712+alex-scp@users.noreply.github.com>
2018-04-02 00:22:40 +0100

    refactoring #1
    added test to the existence of sqlite3
    grouped SQL statements

--- utils/test.sh
+++ utils/test.sh
@@ -11,6 +11,12 @@ if [ $? -ne 0 ]; then
    echo " * expecting a file with name 'yyyy-mm-dd.zip'"
    exit 1
 fi
+which sqlite3 > /dev/null
+if [ $? -ne 0 ]; then
+   echo "'sqlite3' cannot be found, please check that it is installed in your system,"
+   echo "or install it and run this script again"
+   exit 1
+fi
 
 ADSBxchg=`echo $ADSBdb | sed 's/\.zip$//g'`
 
@@ -20,9 +26,13 @@ csv2atsdb -f $ADSBxchg/08/ADS-Bxchg.$ADSBxchg.csv -n ADSBx -t A -b 1M
 #
 # remove all receivers and create a unique receiver with ds_id of '1'
 #
-echo "delete from ds_ads where _rowid_ > '1';" | sqlite3 $ADSBxchg/08/ADS-Bxchg.$ADSBxchg.db
-echo "delete from le_ds where _rowid_ > '1';" | sqlite3 $ADSBxchg/08/ADS-Bxchg.$ADSBxchg.db
-echo "update ds_ads set ds_id = '1';" | sqlite3 $ADSBxchg/08/ADS-Bxchg.$ADSBxchg.db
-echo "update le_ds set ds_id = '1';" | sqlite3 $ADSBxchg/08/ADS-Bxchg.$ADSBxchg.db
-echo "update sd_ads set ds_id = '1';" | sqlite3 $ADSBxchg/08/ADS-Bxchg.$ADSBxchg.db
+cat > /tmp/.rmv_rcvrs << RMV_RCVRS
+delete from ds_ads where _rowid_ > '1';
+delete from le_ds where _rowid_ > '1';
+update ds_ads set ds_id = '1';
+update le_ds set ds_id = '1';
+update sd_ads set ds_id = '1';
+RMV_RCVRS
+sqlite3 $ADSBxchg/08/ADS-Bxchg.$ADSBxchg.db < /tmp/.rmv_rcvrs
+rm /tmp/.rmv_rcvrs
 mv $ADSBxchg/08/ADS-Bxchg.$ADSBxchg.db .
