3fbddd7132881d3db95ed4e53df4aff6624b62ba
Helmut Puhr <helmut.puhr@gmx.at>
2019-01-12 16:18:01 +0100

    added new libs, automatic config&data upgrade

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -15,20 +15,193 @@
  * along with ATSDB.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "global.h"
+#include "logger.h"
+#include "client.h"
+#include "config.h"
+#include "logger.h"
+#include "files.h"
+#include "configurationmanager.h"
+
 #include <QApplication>
 #include <QMessageBox>
 
-#include "logger.h"
-#include "client.h"
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+
+#include <string>
+
+#if USE_EXPERIMENTAL_SOURCE == true
+#include <osgDB/Registry>
+#include "cpl_conv.h"
+#endif
 
 using namespace std;
+using namespace Utils;
+
+namespace po = boost::program_options;
 
 //namespace ATSDB
 //{
 
-Client::Client(int& argc, char ** argv)
+Client::Client(int& argc, char** argv)
     : QApplication(argc, argv)
 {
+    bool reset_config = false;
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+            ("help", "produce help message")
+            //("compression", po::value<int>(), "set compression level")
+            ("reset-config,rc", po::bool_switch(&reset_config), "reset user configuration files")
+            ;
+
+    try
+    {
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+        if (vm.count("help"))
+        {
+            cout << desc << "\n";
+            quit_requested_ = true;
+            return;
+        }
+    }
+    catch (exception& e)
+    {
+        throw runtime_error ("ATSDBClient: unable to parse command line parameters: "+string(e.what()));
+    }
+
+    // check if basic configuration works
+    try
+    {
+        cout << "ATSDBClient: setting directory paths" << endl;
+
+        string system_install_path = SYSTEM_INSTALL_PATH;
+
+#if USE_EXPERIMENTAL_SOURCE == true
+        cout <<"ATSDBClient: includes experimental features" << endl;
+
+        const char* appdir = getenv("APPDIR");
+        if (appdir)
+        {
+            cout << "ATSDBClient: assuming fuse environment in " << appdir << endl;
+            assert (appdir);
+
+            system_install_path = string(appdir) + "/appdir/atsdb/";
+
+            cout << "ATSDBClient: set install path to '" << system_install_path << "'" << endl;
+            assert (Files::directoryExists(system_install_path));
+
+
+            osgDB::FilePathList path_list;
+
+            path_list.push_back("$ORIGIN/appdir/lib");
+            path_list.push_back("$ORIGIN/lib");
+            path_list.push_back("appdir/lib");
+
+            osgDB::Registry::instance()->setLibraryFilePathList(string(appdir) + "/appdir/lib");
+
+            string gdal_path = string(appdir)+"/appdir/atsdb/data/gdal";
+            CPLSetConfigOption("GDAL_DATA", gdal_path.c_str());
+        }
+#endif
+
+        cout << "ATSDBClient: checking if local configuration exists ... ";
+
+        bool config_and_data_exists = Files::directoryExists(HOME_SUBDIRECTORY);
+        bool config_and_data_copied = false;
+
+        if (config_and_data_exists)
+        {
+            cout << " yes" << endl;
+        }
+        else
+        {
+            cout << " no" << endl;
+            copyConfigurationAndData(system_install_path);
+            config_and_data_copied = true;
+        }
+
+        {
+            SimpleConfig config ("main.conf");
+            string config_version;
+
+            if (config.existsId("version"))
+                config_version = config.getString("version");
+
+            if (config_version != VERSION)
+            {
+                cout << "ATSDBClient: configuration mismatch detected, local version '" << config_version << "'"
+                          << " application version '" << VERSION << "'" << endl;
+
+                QMessageBox::StandardButton reply;
+                reply = QMessageBox::question(nullptr, "Upgrade Configuration & Data",
+                                              "A configuration & data updade is required, do you want to update now?",
+                                              QMessageBox::Yes|QMessageBox::No);
+                if (reply == QMessageBox::Yes)
+                {
+                    cout << "ATSDBClient: config & data upgrade confirmed" << endl;
+                    copyConfigurationAndData(system_install_path);
+                }
+                else
+                {
+                    cout << "ATSDBClient: config & data upgrade denied" << endl;
+                    quit_requested_ = true;
+                    return;
+                }
+            }
+        }
+
+        if (reset_config && !config_and_data_copied)
+            copyConfiguration(system_install_path);
+
+        cout << "ATSDBClient: opening simple config file at '" << HOME_CONF_DIRECTORY+"main.conf'" << endl;
+
+        SimpleConfig config ("main.conf");
+        assert (config.existsId("version"));
+        assert (config.existsId("configuration_path"));
+        assert (config.existsId("save_config_on_exit"));
+        assert (config.existsId("log_properties_file"));
+        assert (config.existsId("save_config_on_exit"));
+
+        CURRENT_CONF_DIRECTORY = HOME_CONF_DIRECTORY+config.getString("configuration_path")+"/";
+
+        cout << "ATSDBClient: current configuration path is '" << CURRENT_CONF_DIRECTORY+"'" << endl;
+
+        string log_config_path = HOME_CONF_DIRECTORY+config.getString("log_properties_file");
+        Files::verifyFileExists(log_config_path);
+
+        cout << "ATSDBClient: initializing logger using '" << log_config_path << "'" << endl;
+        Logger::getInstance().init(log_config_path);
+
+        loginf << "ATSDBClient: startup version " << VERSION;
+        string config_version = config.getString("version");
+        loginf << "ATSDBClient: configuration version " << config_version;
+
+        ConfigurationManager::getInstance().init (config.getString("main_configuration_file"));
+    }
+    catch (exception &ex)
+    {
+        logerr  << "ATSDBClient: Caught Exception '" << ex.what() << "'";
+        logerr.flush();
+        //assert (false);
+
+        quit_requested_ = true;
+        return;
+    }
+    catch(...)
+    {
+        logerr  << "ATSDBClient: Caught Exception";
+        logerr.flush();
+        //assert (false);
+
+        quit_requested_ = true;
+        return;
+    }
+
 }
 
 bool Client::notify(QObject * receiver, QEvent * event)
@@ -37,7 +210,7 @@ bool Client::notify(QObject * receiver, QEvent * event)
     {
         return QApplication::notify(receiver, event);
     }
-    catch(std::exception& e)
+    catch(exception& e)
     {
         logerr  << "Client: Exception thrown: " << e.what();
         //assert (false);
@@ -50,4 +223,41 @@ bool Client::notify(QObject * receiver, QEvent * event)
     }
     return false;
 }
+
+bool Client::quitRequested() const
+{
+    return quit_requested_;
+}
+
+void Client::copyConfigurationAndData (const string& system_install_path)
+{
+    if (!Files::directoryExists(system_install_path))
+        throw runtime_error ("ATSDBClient: unable to locate system installation files at '"+system_install_path
+                             +"'");
+
+    cout << "ATSDBClient: copying files from system installation from '" << system_install_path
+              << "' to '" << HOME_SUBDIRECTORY <<  "' ... ";
+
+    if (!Files::copyRecursively(system_install_path, HOME_SUBDIRECTORY))
+        throw runtime_error ("ATSDBClient: copying files from system installation from '" + system_install_path
+                             +"' to '"+HOME_SUBDIRECTORY+" failed");
+
+    cout << " done" << endl;
+}
+
+void Client::copyConfiguration (const string& system_install_path)
+{
+    string system_conf_path = system_install_path+"conf/";
+    string home_conf_path = HOME_SUBDIRECTORY+"conf/";
+
+    cout << "ATSDBClient: reset config from from '" << system_conf_path
+              << "' to '" << home_conf_path <<  "' ... ";
+
+    if (!Files::copyRecursively(system_conf_path, home_conf_path))
+        throw runtime_error ("ATSDBClient: reset config from from '" + system_conf_path
+                             +"' to '" + home_conf_path + "' failed");
+
+    cout << " done" << endl;
+}
+
 //}
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,36 +1,19 @@
 
 #include <QSurfaceFormat>
-
-#include <boost/filesystem.hpp>
-#include <boost/program_options.hpp>
+#include <QMessageBox>
 
 #include <iostream>
 #include <cstdlib>
 
-#include "global.h"
-#include "config.h"
-#include "logger.h"
-#include "configurationmanager.h"
-
 #include "atsdb.h"
 #include "client.h"
 #include "mainwindow.h"
-#include "files.h"
-
-#if USE_EXPERIMENTAL_SOURCE == true
-#include <osgDB/Registry>
-#include "cpl_conv.h"
-#endif
 
 #include <stdio.h>
 #include <execinfo.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-using namespace Utils;
-
-namespace po = boost::program_options;
 
 void handler(int sig) {
   void *array[10];
@@ -40,7 +23,7 @@ void handler(int sig) {
   size = backtrace(array, 10);
 
   // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
+  fprintf(stderr, "error: signal %d:\n", sig);
   backtrace_symbols_fd(array, size, STDERR_FILENO);
   exit(1);
 }
@@ -49,169 +32,15 @@ int main (int argc, char **argv)
 {
     signal(SIGSEGV, handler);   // install our handler
 
-    bool reset_config = false;
-
-    po::options_description desc("Allowed options");
-    desc.add_options()
-        ("help", "produce help message")
-        //("compression", po::value<int>(), "set compression level")
-        ("reset-config,rc", po::bool_switch(&reset_config), "reset user configuration files")
-    ;
-
-    try
-    {
-        po::variables_map vm;
-        po::store(po::parse_command_line(argc, argv, desc), vm);
-        po::notify(vm);
-
-        if (vm.count("help"))
-        {
-            std::cout << desc << "\n";
-            return 1;
-        }
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << "ATSDBClient: unable to parse command line parameters: " << std::endl
-                  << e.what() << std::endl;
-        return 0;
-    }
-
-    // check if basic configuration works
-    try
-    {
-        std::cout << "ATSDBClient: setting directory paths" << std::endl;
-
-        std::string system_install_path = SYSTEM_INSTALL_PATH;
-
-#if USE_EXPERIMENTAL_SOURCE == true
-        std::cout <<"ATSDBClient: includes experimental features" << std::endl;
-
-        const char* appdir = std::getenv("APPDIR");
-        if (appdir)
-        {
-            std::cout << "ATSDBClient: assuming fuse environment in " << appdir << std::endl;
-            assert (appdir);
-
-            system_install_path = std::string(appdir) + "/appdir/atsdb/";
-
-            std::cout << "ATSDBClient: set install path to '" << system_install_path << "'" << std::endl;
-            assert (Files::directoryExists(system_install_path));
-
-
-            osgDB::FilePathList path_list;
-
-            path_list.push_back("$ORIGIN/appdir/lib");
-            path_list.push_back("$ORIGIN/lib");
-            path_list.push_back("appdir/lib");
-
-            osgDB::Registry::instance()->setLibraryFilePathList(std::string(appdir) + "/appdir/lib");
-
-            std::string gdal_path = std::string(appdir)+"/appdir/atsdb/data/gdal";
-            CPLSetConfigOption("GDAL_DATA", gdal_path.c_str());
-        }
-#endif
-
-        std::cout << "ATSDBClient: checking if local configuration exists ... ";
-
-        if (!Files::directoryExists(HOME_SUBDIRECTORY))
-        {
-            std::cout << " no" << std::endl;
-
-            if (!Files::directoryExists(system_install_path))
-            {
-                std::cerr << "ATSDBClient: unable to locate system installation files at '" << system_install_path
-                          << "'" << std::endl;
-                return -1;
-            }
-            std::cout << "ATSDBClient: copying files from system installation from '" << system_install_path
-                      << "' to '" << HOME_SUBDIRECTORY <<  "' ... ";
-            if (!Files::copyRecursively(system_install_path, HOME_SUBDIRECTORY))
-            {
-                std::cout << " failed" << std::endl;
-                return -1;
-            }
-            std::cout << " done" << std::endl;
-        }
-        else
-        {
-            std::cout << " yes" << std::endl;
-        }
-
-        if (reset_config)
-        {
-            std::string system_conf_path = system_install_path+"conf/";
-            std::string home_conf_path = HOME_SUBDIRECTORY+"conf/";
-
-            std::cout << "ATSDBClient: reset config from from '" << system_conf_path
-                      << "' to '" << home_conf_path <<  "' ... ";
-            if (!Files::copyRecursively(system_conf_path, home_conf_path))
-            {
-                std::cout << " failed" << std::endl;
-                return -1;
-            }
-            std::cout << " done" << std::endl;
-        }
-
-        std::cout << "ATSDBClient: opening simple config file at '" << HOME_CONF_DIRECTORY+"main.conf'" << std::endl;
-
-        SimpleConfig config ("main.conf");
-        assert (config.existsId("version"));
-        assert (config.existsId("configuration_path"));
-        assert (config.existsId("save_config_on_exit"));
-        assert (config.existsId("log_properties_file"));
-        assert (config.existsId("save_config_on_exit"));
-
-        CURRENT_CONF_DIRECTORY = HOME_CONF_DIRECTORY+config.getString("configuration_path")+"/";
-
-        std::cout << "ATSDBClient: current configuration path is '" << CURRENT_CONF_DIRECTORY+"'" << std::endl;
-
-        std::string log_config_path = HOME_CONF_DIRECTORY+config.getString("log_properties_file");
-        Files::verifyFileExists(log_config_path);
-
-        std::cout << "ATSDBClient: initializing logger using '" << log_config_path << "'" << std::endl;
-        Logger::getInstance().init(log_config_path);
-
-        loginf << "ATSDBClient: startup version " << VERSION;
-        std::string config_version = config.getString("version");
-        loginf << "ATSDBClient: configuration version " << config_version;
-
-        if (config.existsId("enable_multisampling") && config.getBool("enable_multisampling")
-                && config.existsId("multisampling"))
-        {
-            unsigned int num_samples = config.getUnsignedInt("multisampling");
-            loginf << "ATSDBClient: enabling multisampling with " << num_samples << " samples";
-            //TODO
-//            QSurfaceFormat fmt;
-//            fmt.setSamples(num_samples);
-//            QSurfaceFormat::setDefaultFormat(fmt);
-        }
-
-        ConfigurationManager::getInstance().init (config.getString("main_configuration_file"));
-    }
-    catch (std::exception &ex)
-    {
-        logerr  << "Main: Caught Exception '" << ex.what() << "'";
-        logerr.flush();
-        //assert (false);
-
-        return -1;
-    }
-    catch(...)
-    {
-        logerr  << "Main: Caught Exception";
-        logerr.flush();
-        //assert (false);
-
-        return -1;
-    }
-
     // real atsdb stuff
     try
     {
-        ATSDB::instance().initialize();
-
         Client mf(argc, argv);
+
+        if (mf.quitRequested())
+            return 0;
+
+        ATSDB::instance().initialize();
 
         MainWindow window;
 
@@ -220,9 +49,9 @@ int main (int argc, char **argv)
         return mf.exec();
 
     }
-    catch (std::exception &ex)
+    catch (std::exception& ex)
     {
-        logerr  << "Main: Caught Exception '" << ex.what() << "'";
+        logerr  << "main: caught exception '" << ex.what() << "'";
         logerr.flush();
 
         //assert (false);
@@ -234,7 +63,7 @@ int main (int argc, char **argv)
     }
     catch(...)
     {
-        logerr  << "Main: Caught Exception";
+        logerr  << "main: caught exception";
         logerr.flush();
 
         //assert (false);
