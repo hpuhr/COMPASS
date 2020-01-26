c3f20332e794dea7b9c198811a3684235a91e424
Helmut Puhr <helmut.puhr@gmx.at>
2020-01-26 18:31:22 +0100

    adaptations for old compiler

--- docker/build_jasterix.sh
+++ docker/build_jasterix.sh
@@ -1,6 +1,6 @@
 mkdir -p /app/workspace/jasterix/ub14_build
 cd /app/workspace/jasterix/ub14_build
-cmake -DCMAKE_INSTALL_PREFIX=/usr --config Release ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release ..
 make -j4
 sudo make install
 
