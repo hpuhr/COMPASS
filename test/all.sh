853b58583ba4c86f6e1206d61e429235db1908bc
hpuhr <compass@openats.at>
2022-06-13 08:23:59 +0200

    added test scripts

---
+++ test/all.sh
@@ -0,0 +1,14 @@
+# exit when any command fails
+set -e
+
+echo "App name: $1";
+
+#$1 --reset --quit
+mkdir -p ~/data/test/tmp/
+rm -rf ~/data/test/tmp/*
+
+./test1_eval.sh $1
+./test1_import_lines.sh $1
+./test1_import.sh $1
+./test2_eval.sh $1
+./test3_eval.sh $1
