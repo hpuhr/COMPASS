2ff9da5e14190a7ebd20e00c8b629fb84b5e9a59
hpuhr <compass@openats.at>
2023-12-07 15:03:49 +0100

    eval osgview results improv

--- src/eval/results/mode_c/correctsingle.cpp
+++ src/eval/results/mode_c/correctsingle.cpp
@@ -321,8 +321,8 @@ std::unique_ptr<nlohmann::json::object_t> SingleModeCCorrect::getTargetErrorsVie
         (*viewable_ptr)[VP_POS_LAT_KEY] = (lat_max+lat_min)/2.0;
         (*viewable_ptr)[VP_POS_LON_KEY] = (lon_max+lon_min)/2.0;;
 
-        double lat_w = 1.1*(lat_max-lat_min)/2.0;
-        double lon_w = 1.1*(lon_max-lon_min)/2.0;
+        double lat_w = OSGVIEW_POS_WINDOW_SCALE*(lat_max-lat_min)/2.0;
+        double lon_w = OSGVIEW_POS_WINDOW_SCALE*(lon_max-lon_min)/2.0;
 
         if (lat_w < eval_man_.settings().result_detail_zoom_)
             lat_w = eval_man_.settings().result_detail_zoom_;
