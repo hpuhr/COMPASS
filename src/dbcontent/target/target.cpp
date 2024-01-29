5cb58b34f4c0b3ba8bff931be4732dea72c29f66
hpuhr <compass@openats.at>
2024-01-29 13:12:31 +0100

    fixed duration issues

--- src/dbcontent/target/target.cpp
+++ src/dbcontent/target/target.cpp
@@ -103,7 +103,7 @@ boost::posix_time::time_duration Target::timeDuration() const
 std::string Target::timeDurationStr() const
 {
     if (!time_duration_str_.size())
-        time_duration_str_ = Time::toString(timeDuration());
+        time_duration_str_ = Time::toString(timeDuration(), 0);
 
     return time_duration_str_;
 }
