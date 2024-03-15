a0b95417cc9ae12d464416db64231c5da57de8dc
Philipp Wagner <u_n_a_s@gmx.net>
2024-03-15 17:03:04 +0100

    fixes crash on asterix import in oldos

--- src/task/asynctask.cpp
+++ src/task/asynctask.cpp
@@ -33,7 +33,11 @@
 
 /**
 */
-AsyncTask::AsyncTask() = default;
+AsyncTask::AsyncTask()
+:   task_progress_wrapper_(&task_progress_)
+{
+    connect(&task_progress_wrapper_, &AsyncTaskProgressWrapper::progressChanged, this, &AsyncTask::stateChanged, Qt::ConnectionType::QueuedConnection);
+}
 
 /**
 */
@@ -64,13 +68,10 @@ void AsyncTask::run()
     setState(AsyncTaskState::State::Running);
 
     AsyncTaskResult result(false, "");
-
-    AsyncTaskProgressWrapper pwrapper(&task_progress_);
-    connect(&pwrapper, &AsyncTaskProgressWrapper::progressChanged, this, &AsyncTask::stateChanged);
     
     try
     {
-        result = run_impl(task_state_, pwrapper);
+        result = run_impl(task_state_, task_progress_wrapper_);
     }
     catch(const std::exception& e)
     {
@@ -107,19 +108,16 @@ void AsyncTask::abort()
 
 /**
 */
-bool AsyncTask::runAsyncDialog(AsyncTask* task,
-                               bool auto_close,
+bool AsyncTask::runAsyncDialog(bool auto_close,
                                QWidget* parent)
 {
-    assert(task);
-
-    AsyncTaskDialog dlg(task, auto_close, parent);
+    AsyncTaskDialog dlg(this, auto_close, parent);
 
     auto result = std::async(std::launch::async, 
-        [ task ] () 
+        [ this ] () 
         { 
-            task->run(); 
-            return task->taskState().isDone();
+            this->run(); 
+            return this->taskState().isDone();
         });
 
     dlg.exec();
