214fd9cf9a438a50e86458f972f4440c868a73aa
hpuhr <compass@openats.at>
2024-12-18 10:16:34 +0100

    moved main commands

---
+++ src/client/mainwindow_commands_file.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "rtcommand.h"
+#include "rtcommand_macros.h"
+
+namespace main_window
+{
+// open_db
+struct RTCommandOpenDB : public rtcommand::RTCommand
+{
+    std::string filename_;
+    bool        assure_open_ = false;
+
+    virtual rtcommand::IsValid valid() const override;
+
+protected:
+    virtual bool run_impl() override;
+
+    DECLARE_RTCOMMAND(open_db, "opens existing SQLite3 database with given filename, e.g. ’/data/file1.db’")
+    DECLARE_RTCOMMAND_OPTIONS
+};
+
+// open_recent_db
+struct RTCommandOpenRecentDB : public rtcommand::RTCommand
+{
+    virtual rtcommand::IsValid valid() const override;
+
+protected:
+    virtual bool run_impl() override;
+
+    std::string getPath() const;
+
+    int         index = -1;
+    std::string filename;
+
+    DECLARE_RTCOMMAND(open_recent_db, "opens a SQLite3 database from the recent file history")
+    DECLARE_RTCOMMAND_OPTIONS
+};
+
+// create_db
+struct RTCommandCreateDB : public rtcommand::RTCommand
+{
+    std::string filename_;
+
+    virtual rtcommand::IsValid valid() const override;
+
+protected:
+    virtual bool run_impl() override;
+
+    DECLARE_RTCOMMAND(create_db, "creates and opens new SQLite3 database with given filename, e.g. ’/data/file1.db’")
+    DECLARE_RTCOMMAND_OPTIONS
+};
+
+// close_db
+struct RTCommandCloseDB : public rtcommand::RTCommand
+{
+    bool strict_ = false;
+protected:
+    virtual bool run_impl() override;
+
+    DECLARE_RTCOMMAND(close_db, "closes a currently opened database")
+    DECLARE_RTCOMMAND_OPTIONS
+};
+
+// quit
+struct RTCommandQuit : public rtcommand::RTCommand
+{
+protected:
+    virtual bool run_impl() override;
+
+    DECLARE_RTCOMMAND(quit, "quits the application")
+    DECLARE_RTCOMMAND_NOOPTIONS
+};
+}
