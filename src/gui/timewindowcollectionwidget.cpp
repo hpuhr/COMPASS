4e3363e1c737d351940a844d82db77b9195a20d5
hpuhr <compass@openats.at>
2025-05-15 08:09:11 +0200

    minor improv

--- src/gui/timewindowcollectionwidget.cpp
+++ src/gui/timewindowcollectionwidget.cpp
@@ -13,16 +13,18 @@ using namespace Utils;
 TimeWindowCollectionWidget::TimeWindowCollectionWidget(TimeWindowCollection& collection, QWidget* parent)
     : QWidget(parent), collection_(collection)
 {
-    list_widget_ = new QListWidget(this);
     //list_widget_->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
 
-    add_button_ = new QPushButton("Add", this);
 
     QVBoxLayout* main_layout = new QVBoxLayout();
+
+    list_widget_ = new QListWidget();
     main_layout->addWidget(list_widget_);
 
-    QHBoxLayout* button_layout = new QHBoxLayout(this);
+    QHBoxLayout* button_layout = new QHBoxLayout();
     button_layout->addStretch();
+
+    add_button_ = new QPushButton("Add");
     button_layout->addWidget(add_button_);
 
     main_layout->addLayout(button_layout);
