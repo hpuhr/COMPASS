f076963cc3559f481dbda86731ce0cf670b52301
hpuhr <compass@openats.at>
2025-05-27 09:39:23 +0200

    docker update

--- docker/build_compass.sh
+++ docker/build_compass.sh
@@ -7,9 +7,9 @@ echo "os: '$1'"
 
 export QT_SELECT=5
 
-rm -rf /app/workspace/compass/$1_build # needed since binary becomes too big
-mkdir -p /app/workspace/compass/$1_build
-cd /app/workspace/compass/$1_build
+rm -rf /app/workspace/compass/build_$1 # needed since binary becomes too big
+mkdir -p /app/workspace/compass/build_$1
+cd /app/workspace/compass/build_$1
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo .. # -DCMAKE_PREFIX_PATH=$GL_PATH $CMAKE_OPTS
 make -j $(nproc)
 sudo make install
